2d498b72619c198982bfead4a3356ae20bf550b0
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2020-09-27 21:45:29 -0400

    reads in file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{App, Arg};
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("hexy")
+        .version("0.1.0")
+        .author("geno")
+        .about("colorful hex dump output")
+        .arg(
+            Arg::with_name("hexdump")
+                .short("f")
+                .long("hexdump")
+                .help("file to dump hexily")
+                .takes_value(true),
+        )
+        .get_matches();
+
+
+    if matches.is_present("hexdump") {
+        let mut file = File::open(matches.value_of("hexdump").unwrap().to_string()).unwrap();
+        let mut a = Vec::new();
+        let len = file.read_to_end(&mut a).unwrap();
+        println!("File length: {} bytes", len);
+    } else {
+        println!("Unsupported flag");
+    }
+
+
 }
