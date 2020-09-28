24e51342d8a642b88018a97519e76681f88e330f
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2020-09-27 22:36:05 -0400

    happy with hexdump

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,64 @@ fn main() {
 
     if matches.is_present("hexdump") {
         let mut file = File::open(matches.value_of("hexdump").unwrap().to_string()).unwrap();
-        let mut a = Vec::new();
+        let mut a: Vec<u8> = Vec::new();
         let len = file.read_to_end(&mut a).unwrap();
+
+        colorful_hexdump(&a);
+
         println!("File length: {} bytes", len);
     } else {
         println!("Unsupported flag");
     }
 
+}
 
+fn colorful_hexdump(a: &Vec<u8>) {
+
+    for (n,i) in a.into_iter().enumerate() {
+        if (n % 16) == 0 {
+            if n > 0 {
+                print!("|");
+                for j in n-16..n {
+                    if a[j].is_ascii_graphic() {
+                        print!("{}", a[j] as char);
+                    } else {
+                        print!(".");
+                    }
+                }
+                print!("|");
+            }
+            println!();
+            print!("{:08x} ", n);
+        }
+
+        if (n % 8) == 0 {
+            print!(" ");
+        }
+
+        print!("{:02x} ", i);
+    }
+
+    if (a.len() % 16) != 0 {
+        let mut n = a.len() % 16;
+
+        for _ in 0..(16-n)*3 {
+            print!(" ");
+        }
+        if n <= 8 {
+            n += 1;
+        }
+
+        print!("|");
+        for i in 0..n {
+            if a[a.len()-n+i].is_ascii_graphic() {
+                print!("{}", a[a.len()-n+i] as char);
+            } else {
+                print!(".");
+            }
+        }
+        print!("|");
+    }
+
+    println!();
 }
